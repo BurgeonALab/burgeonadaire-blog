4b9d16f44a55ec64aa676592ea4405dc2eec5db1
Gerald Zandisko <geraldzandisko@burgeonadaire.com>
2024-11-25 11:50:49 +0700

    add schemes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,26 @@
+import { Fragment } from 'react';
 import Link from 'next/link'
+
+const jsonLd = {
+  '@context': 'https://schema.org',
+  '@type': 'WebSite',
+  'name': 'Burgeon Adaire Blog',
+  'alternateName': 'Our Minds Inside Burgeon Adaire',
+  'url': 'https://blog.burgeonadaire.com'
+}
 
 export default function Home() {
   return (
-    <div className="flex flex-col justify-center items-center h-screen">
-      <p className="font-medium text-lg sm:text-2xl">Burgeon Adaire <span className="font-normal">Blog</span></p>
+    <Fragment>
+      <div className="flex flex-col justify-center items-center h-screen">
+        <p className="font-medium text-lg sm:text-2xl">Burgeon Adaire <span className="font-normal">Blog</span></p>
 
-      <Link className='blog-link' href="https://burgeonadaire.com" target='_blank' rel='noopener'>
-        <p className="mt-3 font-normal">Coming Soon</p>
-      </Link>
-    </div>
+        <Link className='blog-link' href="https://burgeonadaire.com" target='_blank' rel='noopener'>
+          <p className="mt-3 font-normal">Coming Soon</p>
+        </Link>
+      </div>
+
+      <script type="application/ld+json" dangerouslySetInnerHTML={{ __html: JSON.stringify(jsonLd) }} />
+    </Fragment>
   );
 }
